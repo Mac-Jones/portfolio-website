fce9fa1ddfca507ccebcc543f0cc6daddc09aa24
macjonessoriano <sorianomacjones@gmail.com>
2023-04-13 12:05:01 +0800

    added navigation

--- src/components/sidebar/sidebar.component.jsx
+++ src/components/sidebar/sidebar.component.jsx
@@ -1,9 +1,35 @@
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { useRef } from 'react';
+
+// import MJLogo from '../../img/mj-logo-transparent.png';
+import MJLogo from '../../img/mj-logo-primary-transparent.png';
+
 import './sidebar.styles.scss';
 
 const Sidebar = () => {
+	const navRef = useRef();
+
+	const showNavBar = () => {
+		console.log('haha');
+		navRef.current.classList.toggle('responsive_nav');
+	};
+
 	return (
 		<div className='sidebar'>
-			<button className='nav-btn'></button>
+			<img src={MJLogo} alt='Mac Jones Logo' className='sidebar__logo' />
+			<nav ref={navRef}>
+				<a href='/#'>Home</a>
+				<a href='/#'>Story</a>
+				<a href='/#'>Skills</a>
+				<a href='/#'>Projects</a>
+				<a href='/#'>Footer</a>
+				<button className='nav-btn nav-close-btn'>
+					<FaTimes onClick={showNavBar} />
+				</button>
+			</nav>
+			<button className='nav-btn'>
+				<FaBars onClick={showNavBar} />
+			</button>
 		</div>
 	);
 };
